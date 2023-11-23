cebba1d14146cd1cd3b95585883768e335149062
Alvaro <alvarofernandezdelalamo@gmail.com>
2023-11-23 14:14:11 +0100

    Nuevas sentencias SQL asociados a la actividad 5.4

--- script.sql
+++ script.sql
@@ -1,93 +1,180 @@
--- Crear la BBDD
-CREATE DATABASE IF NOT EXISTS compania_aerea;
+-- Script asociado a la Actividad 5.3
 
--- Para operar con la BBDD
-USE compania_aerea;
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS compania_aerea_inm;
 
+-- Conexión a las base de datos.
+use compania_aerea_inm;
 
--- T_PASAJERO
+/* Borrado de todas las tablas */
+DROP TABLE IF EXISTS T_PASAJERO_VUELO;
+DROP TABLE IF EXISTS T_TELEFONO_PASAJERO;
+DROP TABLE IF EXISTS T_PASAJERO;
+DROP TABLE IF EXISTS T_TRIPULANTE_VUELO;
+DROP TABLE IF EXISTS T_VUELO;
+DROP TABLE IF EXISTS AVION;
+DROP TABLE IF EXISTS T_TRIPULANTE;
+DROP TABLE IF EXISTS T_TELEFONO_TRABAJADOR;
+DROP TABLE IF EXISTS T_TRABAJADOR;
+DROP TABLE IF EXISTS T_CATEGORIA;
+
+-- Creación de tablas
+-- TABLA T_PASAJERO
 CREATE TABLE IF NOT EXISTS T_PASAJERO(
-  DNI CHAR(9) PRIMARY KEY NOT NULL,
+  DNI CHAR(9) NOT NULL PRIMARY KEY,
   Nombre VARCHAR(50) NOT NULL,
-  Apellidos VARCHAR (100) NOT NULL,
-  Email VARCHAR (100) NOT NULL
+  Apellidos VARCHAR(150) NOT NULL
 );
 
--- T_TELEFONO_PASAJERO
+-- TABLA T_TELEFONO_PASAJERO
 CREATE TABLE IF NOT EXISTS T_TELEFONO_PASAJERO(
-  DNI_Pasajero CHAR(9) NOT NULL,
-  Telefono TINYINT NOT NULL,
-  PRIMARY KEY(DNI_Pasajero, Telefono),
-  CONSTRAINT FK_PASAJERO_TELEFONO
-    FOREIGN KEY (DNI_Pasajero) REFERENCES T_PASAJERO(DNI)
+  DNI CHAR(9) NOT NULL,
+  Telefono INT NOT NULL,
+  PRIMARY KEY (DNI, Telefono),
+  CONSTRAINT FK_TELPASAJERO_PASAJERO
+    FOREIGN KEY (DNI) REFERENCES T_PASAJERO(DNI)
 );
 
--- T_AVION
+-- TABLA AVION
 CREATE TABLE IF NOT EXISTS T_AVION(
-  Matricula TINYINT PRIMARY KEY NOT NULL,
-  Fabricante VARCHAR (50) NOT NULL,
-  Modelo VARCHAR (50) NOT NULL,
-  Capacidad TINYINT NOT NULL,
+  Matricula VARCHAR(10) NOT NULL PRIMARY KEY,
+  Fabricante VARCHAR(250) NOT NULL,
+  Modelo VARCHAR(250) NOT NULL,
+  Capacidad INT NOT NULL,
   Autonomia INT NOT NULL
 );
 
--- T_VUELO
+-- TABLA T_VUELO
 CREATE TABLE IF NOT EXISTS T_VUELO(
-  Id_Vuelo INT(9) PRIMARY KEY NOT NULL,
-  Matricula_Avion TINYINT NOT NULL,
-  Origen VARCHAR (50) NOT NULL,
-  Destino VARCHAR (50) NOT NULL,
-  Fecha DATE NOT NULL,
+  IdVuelo INT NOT NULL PRIMARY KEY,
+  FechaInicio DATE,
+  AeropuertoOrigen VARCHAR(50) NOT NULL,
+  AeropuertoDestino VARCHAR(50) NOT NULL,
+  Matricula VARCHAR(10) NOT NULL ,
   CONSTRAINT FK_VUELO_AVION
-    FOREIGN KEY (Matricula_Avion) REFERENCES T_AVION(Matricula)
+    FOREIGN KEY (Matricula) REFERENCES T_AVION(Matricula)
 );
 
--- T_DISTRIBUCION
-CREATE TABLE IF NOT EXISTS T_DISTRIBUCION(
-  DNI_Pasajero CHAR(9) NOT NULL,
-  Id_Vuelo INT(9) NOT NULL,
-  Asiento CHAR(6) NOT NULL,
-  Clase VARCHAR(50) NOT NULL,
-  PRIMARY KEY(DNI_Pasajero, Id_Vuelo),
-  CONSTRAINT FK_DISTRIBUCION_PASAJERO
-    FOREIGN KEY (DNI_Pasajero) REFERENCES T_PASAJERO(DNI),
-  CONSTRAINT FK_DISTRIBUCION_VUELO
-    FOREIGN KEY (Id_Vuelo) REFERENCES T_VUELO(Id_Vuelo)
+-- TABLA T_PASAJERO_VUELO
+CREATE TABLE IF NOT EXISTS T_PASAJERO_VUELO(
+  DNI CHAR(9) NOT NULL,
+  IdVuelo INT NOT NULL,
+  Clase ENUM ('Business', 'Primera', 'Turista'),
+  Asiento SMALLINT NOT NULL,
+  PRIMARY KEY (DNI, IdVuelo),
+  CONSTRAINT FK_PASAJEROVUELO_PASAJERO
+    FOREIGN KEY (DNI) REFERENCES T_PASAJERO (DNI),
+  CONSTRAINT FKPASAJEROVUELO_VUELO
+    FOREIGN KEY (IdVuelo) REFERENCES T_VUELO(IdVuelo)
 );
 
--- T_CAT_PROFESIONAL
-CREATE TABLE IF NOT EXISTS T_CAT_PROFESIONAL(
-  Cod_Categoria CHAR(9) PRIMARY KEY NOT NULL,
+-- TABLA T_CATEGORIA
+CREATE TABLE IF NOT EXISTS T_CATEGORIA(
+  IdCategoria INT NOT NULL PRIMARY KEY,
   Nombre VARCHAR(50) NOT NULL
 );
 
--- T_PERSONAL
-CREATE TABLE IF NOT EXISTS T_PERSONAL(
-  Id_Trabajador INT(9) PRIMARY KEY NOT NULL,
-  Cod_Categoria CHAR(9) NOT NULL,
+-- TABLA T_TRABAJADOR
+CREATE TABLE IF NOT EXISTS T_TRABAJADOR(
+  IdTrabajador INT NOT NULL PRIMARY KEY,
   Nombre VARCHAR(50) NOT NULL,
   Apellido1 VARCHAR(100) NOT NULL,
   Apellido2 VARCHAR(100) NULL,
-  CONSTRAINT FK_PERSONAL_CATEGORIA
-    FOREIGN KEY (Cod_Categoria) REFERENCES T_CAT_PROFESIONAL(Cod_Categoria)
+  IdCategoria INT NOT NULL,
+  CONSTRAINT FK_TRABAJADOR_CATEGORIA
+     FOREIGN KEY (IdCategoria) REFERENCES T_CATEGORIA(IdCategoria)
 );
 
--- T_PUESTO
-CREATE TABLE IF NOT EXISTS T_PUESTO(
-  Id_Trabajador INT(9) NOT NULL,
-  Id_Vuelo INT(9) NOT NULL,
+-- TABLA T_TELEFONO_TRABAJADOR
+CREATE TABLE IF NOT EXISTS T_TELEFONO_TRABAJADOR(
+  IdTrabajador INT NOT NULL,
+  Telefono INT NOT NULL,
+  PRIMARY KEY (IdTrabajador, Telefono),
+  CONSTRAINT FK_TELTRABAJADOR_TRABAJADOR
+    FOREIGN KEY (IdTrabajador) REFERENCES T_TRABAJADOR(IdTrabajador)
+);
+
+
+
+-- TABLA T_TRIPULANTE
+CREATE TABLE IF NOT EXISTS T_TRIPULANTE (
+  IdTripulante INT NOT NULL PRIMARY KEY,
+  FechaInicio DATE NOT NULL,
+  IdTrabajador INT NOT NULL UNIQUE,
+  CONSTRAINT FK_TRIPULANTE_TRABAJADOR
+    FOREIGN KEY(IdTrabajador) REFERENCES T_TRABAJADOR(IdTrabajador)
+);
+
+-- TABLA T_TRIPULANTE_VUELO
+CREATE TABLE IF NOT EXISTS T_TRIPULANTE_VUELO(
+  IdTripulante INT NOT NULL,
+  IdVuelo INT NOT NULL,
   Puesto VARCHAR(50) NOT NULL,
-  CONSTRAINT FK_PUESTO_PERSONAL
-    FOREIGN KEY (Id_Trabajador) REFERENCES T_PERSONAL(Id_Trabajador),
-  CONSTRAINT FK_PUESTO_VUELO
-    FOREIGN KEY (Id_Vuelo) REFERENCES T_VUELO(Id_Vuelo)
+  PRIMARY KEY(idTripulante, IdVuelo),
+  CONSTRAINT FK_TRIPULANTEVUELO_TRIPULANTE
+    FOREIGN KEY (IdTripulante) REFERENCES T_TRIPULANTE (IdTripulante),
+  CONSTRAINT FK_TRIPULANTEVUELO_VUELO
+    FOREIGN KEY (IdVuelo) REFERENCES T_VUELO(IdVuelo)
 );
 
--- T_TELEFONO_PERSONAL
-CREATE TABLE IF NOT EXISTS T_TELEFONO_PERSONAL(
-  Id_Trabajador INT(9) NOT NULL,
-  Telefono TINYINT NOT NULL,
-  PRIMARY KEY(Id_Trabajador, Telefono),
-  CONSTRAINT FK_TELEFONO_TRABAJADOR
-    FOREIGN KEY (Id_Trabajador) REFERENCES T_PERSONAL(Id_Trabajador)
+
+-- Cambios asociados a la actividad 5.4
+-- La base de datos tendrá como como “conjunto de caracteres” utf8mb4 y utf8mb4_unicode_ci  como “cotejamiento”. 
+ALTER DATABASE compania_aerea_inm CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+-- Se desea que la información sobre la capacidad del avión sea un campo numérico sin signo y con valor máximo de 450
+ALTER TABLE T_AVION MODIFY COLUMN Capacidad SMALLINT UNSIGNED NOT NULL;
+ALTER TABLE T_AVION ADD CONSTRAINT CHK_Capacidad CHECK(Capacidad<=480);
+
+
+/*
+ Se desea que la información sobre la autonomía de vuelo sean un campo numérico sin signo, 
+con un valor máximo de 48 horas. Además, se desea el campo autonomía de vuelo que sea opcional.
+*/
+ALTER TABLE T_AVION MODIFY Autonomia TINYINT UNSIGNED NULL;
+ALTER TABLE T_AVION ADD CONSTRAINT CHK_Autonomia CHECK(Autonomia <=48);
+
+
+-- De los trabajadores se desea conocer los apellidos, siendo el segundo de ellos opcional.
+-- No hay que hacer nada. Así está en la activididad 5.3
+
+
+-- Se validará que el la columna asociado al asiento que ocupa el pasajero sea mayor que 0 (usando la restricción CHECK).
+ALTER TABLE T_PASAJERO_VUELO ADD CONSTRAINT CHK_Asiento CHECK(Asiento>0); 
+
+/*
+Del tipo de la clase del asiento que ocupa el pasajero (turista, primera o business), 
+se quiere almacenar información del identificador del Tipo;
+ este identificador será un campo numérico autoincremental. 
+*/
+-- 1º Crear Tabla T_CLASE
+CREATE TABLE IF NOT EXISTS T_CLASE(
+  IdClase TINYINT UNSIGNED NOT NULL, -- Identificador con valores positivos (pocos valores)
+  Nombre VARCHAR(250) NOT NULL,
+  PRIMARY KEY(IdClase)
 );
+-- 2º Crear una columna IdClase en la tabla T_PASAJERO_VUELO
+ALTER TABLE T_PASAJERO_VUELO ADD COLUMN IdClase TINYINT UNSIGNED NOT NULL;
+-- 3º Se Crear una restricción de tipo FOREIGN KEY sobre T_CLASE
+ALTER TABLE T_PASAJERO_VUELO ADD CONSTRAINT FK_PASAJEROVUELO_CLASE
+  FOREIGN KEY (IdClase) REFERENCES T_CLASE (IdClase);
+-- 4º Se elimina la columna Clase
+ALTER TABLE T_PASAJERO_VUELO DROP COLUMN Clase;
+
+
+-- Se cambiará el nombre de la tabla asociada a la entidad AVION al nombre T_AEROPLANO.
+-- Renombrar la tabla T_AVION
+ALTER TABLE T_AVION RENAME T_AEROPLANO;
+-- Renombrar en T_VUELO la FK asociada a AVION (Matricula). Renombrar = Borar (FK_VUELO_AVION) + Crear (FK_VUELO_AEROPLANO)
+ALTER TABLE T_VUELO DROP CONSTRAINT FK_VUELO_AVION;
+ALTER TABLE T_VUELO ADD CONSTRAINT FK_VUELO_AEROPLANO
+  FOREIGN KEY (Matricula) REFERENCES T_AEROPLANO (Matricula);
+
+/* Pruebas restricciones
+INSERT INTO T_AEROPLANO VALUES('000012R', 'AIRBUS', 'Airbus A318', '550', '480');
+INSERT INTO T_VUELO VALUES (1, NULL, 'Madrid', 'Barcelona', '000012R');
+INSERT INTO T_PASAJERO VALUES ('13256354K', 'Adela', 'Sánchez Romina');
+INSERT INTO T_CLASE VALUES (1, 'Business');
+INSERT INTO T_PASAJERO_VUELO VALUES ('13256354K', 1, -6, 1);
+*/
